11ef26fa41e41c570633b96e38f8ff8e53be0c21
Aniket205kadam <aniketrkadam205@gmail.com>
2024-12-29 16:37:00 +0530

    redux toolkit🛰️

--- todoUsingReduxToolkit/src/components/AddTodo.jsx
+++ todoUsingReduxToolkit/src/components/AddTodo.jsx
@@ -4,10 +4,16 @@ import { addTodo } from '../features/todo/todoSlice';
 
 function AddTodo() {
   const [input, setInput] = useState('');
+  const [error, setError] = useState(false);
   const dispatch = useDispatch();
 
   const addTodoHandler = (event) => {
     event.preventDefault();
+    if(input === '') {
+      setError(true);
+      return;
+    }
+    setError(false);
     dispatch(addTodo(input));
     // clear the input
     setInput('');
@@ -18,7 +24,9 @@ function AddTodo() {
     <form onSubmit={addTodoHandler} className="space-x-3 mt-12 m-4">
       <input
         type="text"
-        className="text-xl w-96 h-16 bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+        className={`text-xl w-96 h-16 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out
+          ${error ? "bg-red-500 border-red-500 animate-shake" : "bg-gray-800"}  
+        `}
         placeholder="Enter a Todo..."
         value={input}
         onChange={(e) => setInput(e.target.value)}
